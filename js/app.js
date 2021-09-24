b110edfacbb50318c44558992bdceacb1c1c4da5
naymyochay <naymyoaung.atechnician@gmail.com>
2021-09-23 23:20:45 -0800

    first-commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var main = document.getElementsByTagName('div');
+var btn = document.getElementsByTagName('button');
+
+btn[0].addEventListener("click", function (){
+	main[0].style.display = "none";
+});
